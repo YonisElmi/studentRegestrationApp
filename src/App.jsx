283eea07d5c7edb3638e7456413b17068fe39ff6
YonisElmi <yoonisnuur76@gmail.com>
2024-07-17 13:54:38 +0300

    Header

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Header from './components/Header';
+
+
+function App() {
+  return (
+    <Router>
+      <Header />
+      
+    </Router>
+  );
+}
+
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { StudentProvider } from './contexts/StudentContext';
 
 ReactDOM.render(
-  <StudentProvider>
+  
     <App />
-  </StudentProvider>,
+ ,
   document.getElementById('root')
 );
