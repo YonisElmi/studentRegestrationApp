478e196bca571da89022a9ac786206592f9a1380
YonisElmi <yoonisnuur76@gmail.com>
2024-07-17 17:56:03 +0300

    Home

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
+import Home from './pages/Home';
 
 
 function App() {
   return (
     <Router>
       <Header />
+      <Home/>
       
     </Router>
   );
