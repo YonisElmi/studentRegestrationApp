3faeff1b3e8ff2354538fae0bcc5b93b1d4a5df9
YonisElmi <yoonisnuur76@gmail.com>
2024-07-18 09:17:47 +0300

    .

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import About from './pages/About';
 function App() {
   return (
     <Router>
-      <Header />
+      <Header/>
       <Home/>
       <About/>
       
---
+++ src/components/StudentForm.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+const StudentForm = () => {
+
+    return(
+        <P>Students Form</P>
+    )
+ 
+
+};
+
+export default StudentForm;
