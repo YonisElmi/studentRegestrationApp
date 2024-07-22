2fd66f18ff629c0dba47de20c482b33b88be318d
YonisElmi <yoonisnuur76@gmail.com>
2024-07-22 17:10:15 +0300

    .

---
+++ src/components/StudentList.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const StudentList = () => {
+    return(
+        <h1>Students List</h1>
+    )
+}
+
+export default StudentList;
---
+++ src/pages/ViewStudents.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import StudentList from "../components/StudentList";
+
+const ViewStudents = () => {
+    return(
+        <StudentList/>
+    )
+}
+
+export default ViewStudents;
