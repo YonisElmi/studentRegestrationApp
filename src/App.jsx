bd9f41b99ee63ca091d4ac27767649e4507d0189
YonisElmi <162869659+YonisElmi@users.noreply.github.com>
2024-07-27 20:07:49 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,24 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
 import Home from './pages/Home';
-import About from './pages/About';
 import Register from './pages/Register';
-
+import ViewStudents from './pages/ViewStudents';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Footer from './components/Footer';  
 
 function App() {
   return (
     <Router>
-      <Header/>
-      <Home/>
-      <About/>
-      <Register/>
-      
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/view-students" element={<ViewStudents />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+      <Footer /> 
     </Router>
   );
 }
