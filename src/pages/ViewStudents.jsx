933f8a9e94832a6730911a5696ec56df75f79a32
YonisElmi <162869659+YonisElmi@users.noreply.github.com>
2024-07-28 18:56:24 +0300

    Update ViewStudents.jsx

--- src/pages/ViewStudents.jsx
+++ src/pages/ViewStudents.jsx
@@ -1,10 +1,13 @@
-import React from "react";
-import StudentList from "../components/StudentList";
+import React from 'react';
+import StudentList from '../components/StudentList';
 
 const ViewStudents = () => {
-    return(
-        <StudentList/>
-    )
-}
+  return (
+    <div className="text-center mt-10">
+      <h1 className="text-4xl font-bold mb-6">Student List</h1>
+      <StudentList />
+    </div>
+  );
+};
 
 export default ViewStudents;
