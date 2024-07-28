db08ddf062455e1d08af017e4e68e809ada83b79
YonisElmi <162869659+YonisElmi@users.noreply.github.com>
2024-07-28 18:59:53 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { StudentProvider } from './contexts/StudentContext';
 
 ReactDOM.render(
-  
+  <StudentProvider>
     <App />
- ,
+  </StudentProvider>,
   document.getElementById('root')
 );
