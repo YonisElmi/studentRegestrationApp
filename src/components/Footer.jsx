9a7938b4c9c9dd935caff977580231dace82dd27
YonisElmi <162869659+YonisElmi@users.noreply.github.com>
2024-07-29 10:10:50 +0300

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,6 @@
 
 import React from 'react';
+import logo from '../images/logo2.png'
 
 const Footer = () => {
   return (
@@ -7,7 +8,7 @@ const Footer = () => {
       <div className="px-4 mx-auto sm:px-6 lg:px-8 max-w-7xl">
         <div className="grid grid-cols-2 md:col-span-3 lg:grid-cols-6 gap-y-16 gap-x-12">
           <div className="col-span-2 md:col-span-3 lg:col-span-2 lg:pr-8">
-          <img src="../assets/logo2.png" className="h-20" alt="Logo" />
+          <img src={logo} className="h-20" alt="Logo" />
           
             <p className="text-base leading-relaxed text-gray-600 mt-7">Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit.</p>
             <ul className="flex items-center space-x-3 mt-9">
